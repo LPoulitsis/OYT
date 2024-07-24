c776d5a7a75bf6c06d1bc8df87bf49952a7e66f8
Lefteris Poulitsis <l.poulitsis@gmail.com>
2024-07-24 21:11:08 +0300

    Add function to make the transcript text based.

--- app/oyt.py
+++ app/oyt.py
@@ -10,3 +10,27 @@ The chatbot then generates a context based on the transcript and the user can st
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import ollama
+
+def format_video_transcript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatter = TextFormatter()
+    return formatter.format_transcript(transcript)
+
+if __name__ == "__main__":
+    logo = """
+  _____   _______ 
+ / _ \\ \\ / /_   _|
+| | | \\ V /  | |  
+| |_| || |   | |  
+ \\___/ |_|   |_|  
+    """
+    print(logo)
+    
+    while True:
+
+        video_id = input("Enter a video ID (or press 'x' to exit): ")
+        
+        if video_id == 'x':
+            break
+        
+        text_formatted = format_video_transcript(video_id)
