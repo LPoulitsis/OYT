94dad6dbb6bb10ab5fe03ca918495fabe72ccf28
Lefteris Poulitsis <l.poulitsis@gmail.com>
2024-07-24 21:13:19 +0300

    Generate the context needed for the LLM to give the most valid responses.

--- app/oyt.py
+++ app/oyt.py
@@ -11,6 +11,20 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import ollama
 
+def generate_context(text_formatted):
+    ollama.chat(model = "mistral", messages = [
+        {
+            "role": "user",
+            "content": f"""You will be provided with a video 
+                        transcript from youtube which you will 
+                        have to answer certain questions to 
+                        the user based on the transcript contents.
+                        Act as an expert in any given field related
+                        to the video transcript's topics. 
+                        Here's the transcript: {text_formatted}"""
+        }
+    ])
+
 def format_video_transcript(video_id):
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     formatter = TextFormatter()
@@ -34,3 +48,6 @@ if __name__ == "__main__":
             break
         
         text_formatted = format_video_transcript(video_id)
+        
+        print("Transcript has been saved successfully!")
+        generate_context(text_formatted)
