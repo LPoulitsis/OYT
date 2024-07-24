b71f973a7d93602c6ec8dcfce02459789d470d7f
Lefteris Poulitsis <l.poulitsis@gmail.com>
2024-07-24 22:10:16 +0300

    Improve the logo and the colors of the print statements. Fix bug with id check before exit check.

--- app/oyt.py
+++ app/oyt.py
@@ -10,10 +10,12 @@ The chatbot then generates a context based on the transcript and the user can st
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import ollama
+from termcolor import colored
 
 def generate_response():
     while True:
-        question = input("Question: ")
+        question = input(colored("Question: ", "green"))
+        print()
         if question == 'bye':
             break
         response = ollama.chat(model = "mistral", messages = [
@@ -22,7 +24,7 @@ def generate_response():
                 "content": question
             }
         ])
-        print(f"Response: {response['message']['content']}")
+        print(f"{colored("Response:", "blue")} {response['message']['content']}\n")
 
 def generate_context(text_formatted):
     ollama.chat(model = "mistral", messages = [
@@ -44,30 +46,35 @@ def format_video_transcript(video_id):
     return formatter.format_transcript(transcript)
 
 if __name__ == "__main__":
-    logo = """
-  _____   _______ 
- / _ \\ \\ / /_   _|
-| | | \\ V /  | |  
-| |_| || |   | |  
- \\___/ |_|   |_|  
-    """
-    print(logo)
+    logo = f"""
+  {colored("___", "white")}{colored("__   __", "red")}{colored("_____", "yellow")}
+ {colored("/ _ \\", "white")} {colored("\\ / /", "red")}{colored("_   _|", "yellow")}
+{colored("| | | \\", "white")} {colored("V /", "red")}  {colored("| |", "yellow")}
+{colored("| |_| |", "white")}{colored("| |", "red")}   {colored("| |", "yellow")}
+{colored(" \\___/", "white")} {colored("|_|", "red")}   {colored("|_|", "yellow")}  
+    """    
+    
+    print(colored(logo, "red"))
+    
+    print(f"""{colored("Welcome", "green")} to the {colored("Ollama", "white")} {colored("YouTube", "red")} {colored("Transcript", "yellow")} chatbot!
+This chatbot generates responses based on a video transcript from YouTube.\n""")
     
     while True:
 
         video_id = input("Enter a video ID (or press 'x' to exit): ")
+        
+        if video_id == 'x':
+            break
         
         # Check if video_id is a valid YouTube video ID
         if len(video_id) != 11:
             print("Invalid video ID. Please try again.")
             continue
         
-        if video_id == 'x':
-            break
-        
         text_formatted = format_video_transcript(video_id)
         
-        print("Transcript has been saved successfully!")
+        print(colored("Transcript has been saved successfully!", "green"))
+        print(colored("Please wait while we prepare the model...", "yellow"))
         generate_context(text_formatted)
-        print("You can now start questioning the model. You can exit any time by typing 'bye'.")
+        print("You can now start questioning the model. You can exit any time by typing 'bye'.\n")
         generate_response()
